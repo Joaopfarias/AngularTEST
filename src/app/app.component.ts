52afd7d8b9aa6e2e7e9ca81ddc28e8c165e99a61
joaopfarias <joaop210601@gmail.com>
2022-07-02 15:59:39 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSort } from '@angular/material/sort';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+
+export interface PeriodicElement {
+  nome: string;
+  position: number;
+  telefone: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'TesteAngular';
+export class AppComponent implements AfterViewInit {
+  displayedColumns: string[] = ['position', 'nome', 'telefone'];
+  data:any = [];
+  position:number = 0;
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  formTeste: FormGroup = this.fb.group({
+    nome: ['', Validators.required],
+    telefone: ['', Validators.required]
+  })
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<PeriodicElement>;
+  constructor(private fb: FormBuilder) { }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  ngOnInit(): void {
+    // this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  add() {
+    if (this.formTeste.valid) {
+      this.data.push({
+        position: this.position++,
+        nome: this.formTeste.value.nome,
+        telefone: this.formTeste.value.telefone
+      });
+      this.dataSource = new MatTableDataSource<PeriodicElement>(this.data);
+      this.dataSource.sort = this.sort;
+    }
+  }
+  removeData() {
+    this.dataSource.data.pop();
+    this.dataSource.sort = this.sort;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
